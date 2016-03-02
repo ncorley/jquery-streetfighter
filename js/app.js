ba4f394a725a70265965aa47d003f853ad14bb13
ncorley <nyleva.corley@blackbaud.com>
2016-03-02 12:55:47 -0600

    Updated main.css, app.js and main.html

--- js/app.js
+++ js/app.js
@@ -3,31 +3,42 @@ $(document).ready(function() {
 	$(".ryu-still").hide();
 	$(".ryu-ready").show();
 	})
-	.mouseleave(function () {
+
+	.mouseleave(function() {
 	$(".ryu-ready").hide();
 	$(".ryu-still").show();
 	})
+
 	.mousedown(function() {
   		playHadouken();
   		$(".ryu-ready").hide();
   		$(".ryu-throwing").show();
-  		$(".hadouken").finish().show()
-  			.animate(
+  		$(".hadouken").finish().show().animate(
     		{"left": "1070px"},
     		500,
     		function() {
       		$(this).hide();
       		$(this).css("left", "570px");
-    	}
-  		);
+    	});
 	})
-	.mouseup(function () {
+
+	.mouseup(function() {
 	$(".ryu-throwing").hide();
 	$(".ryu-ready").show();
+	})
+
+	.keydown(function(event) {
+        if (event.which == 88) {
+        $(".ryu-still").hide();
+        $(".ryu-cool").show();
+    };
 	});
+
 });
+
 	function playHadouken () {
   	$("#hadouken-sound")[0].volume = 0.5;
  	$("#hadouken-sound")[0].load();
   	$("#hadouken-sound")[0].play();
 }
+
